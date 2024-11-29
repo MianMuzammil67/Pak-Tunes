689e690644e254693523b37987fd7b3553adbc96
mian muzammil <mianmuzammil30@gmail.com>
2024-11-29 05:50:33 +0500

    fixes some ui issues

--- app/src/main/java/com/example/paktunes/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/example/paktunes/adapter/CategoryAdapter.kt
@@ -1,8 +1,8 @@
 package com.example.paktunes.adapter
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -16,24 +16,26 @@ class CategoryAdapter(
 ) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
      private var _itemClicked: ((Int) -> Unit)? = null
-    private val colors = listOf("#678026", "#9854B2", "#CF4321", "#3371E4")
-
+    private lateinit var colors: List<Int>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        colors = listOf(
+            ContextCompat.getColor(parent.context, R.color.olive_green),
+            ContextCompat.getColor(parent.context, R.color.lavender_purple),
+            ContextCompat.getColor(parent.context, R.color.coral_red),
+            ContextCompat.getColor(parent.context, R.color.sky_blue)
+        )
         return ViewHolder(
             CategoryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val category = diffUtil.currentList[position]
-
-
 
         holder.binding.apply {
             val color = getColorForPosition(position)
 
-            clParent.setBackgroundColor(Color.parseColor(color))
+            clParent.setBackgroundColor(color)
             tvCatName.text = category.name
         }
         holder.itemView.apply {
@@ -73,7 +75,7 @@ class CategoryAdapter(
         _itemClicked = listener
     }
 
-    private fun getColorForPosition(position: Int): String {
+    private fun getColorForPosition(position: Int): Int {
         return if (recyclerViewType == 0) {
             // RecyclerView 1 uses the color list normally
             colors[position % colors.size]
